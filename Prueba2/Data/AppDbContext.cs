26c3f6da9b48bfdb82fbd1fcfd53426dc21b064a
Ana Camila Lopez Correa <a2024112021@estudiantes.upsa.edu.bo>
2025-09-14 22:02:58 -0400

    Primer commit helicoptero

--- Prueba2/Data/AppDbContext.cs
+++ Prueba2/Data/AppDbContext.cs
@@ -3,6 +3,9 @@ using Prueba2.Models;
 
 namespace Prueba2.Data;
 
-public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+public class AppDbContext : DbContext
 {
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Helicoptero> Helicoptero => Set<Helicoptero>();
 }
